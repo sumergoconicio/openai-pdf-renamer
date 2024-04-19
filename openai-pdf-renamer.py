9eca5d69261447c1a1e055327972c1b3b509e3cf
Naveen Srivatsav <srivatsa@gmail.com>
2024-04-19 12:53:11 +0200

    Update openai-pdf-renamer.py
    Added header

--- openai-pdf-renamer.py
+++ openai-pdf-renamer.py
@@ -1,4 +1,11 @@
-# This script takes a folder full of badly-named PDFs, extracts text from the first page, sends that text to OpenAI and renames files with the results
+##############
+# Name: openai-pdf-renamer
+# Author: Naveen Srivatsav
+# Version: 0.1
+# Description: This script takes a folder full of badly-named PDFs, 
+# extracts text from the first page, sends that text to OpenAI and renames files with the results.
+#############
+
 
 # libraries
 import os, re, time
